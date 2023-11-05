4264d867257255a17fb9477f3c31664b097dfc1e
jpblicer <jpblicer@gmail.com>
2023-11-05 21:15:37 +0900

    Alternative turns which change between x and o

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-const player={
-    marker : "x",
+const player1={
+    marker : "X",
 }
+
+const player2={
+    marker : "O",
+}
+
+
+function playerTurn(){
+    if(roundNumber%2){
+        return player1.marker
+    }else{
+        return player2.marker
+    }
+}
+
 
 const boxElements = document.querySelectorAll(".box");
 
 boxElements.forEach(box => {
     box.addEventListener("click", setMarker);
 
+    roundNumber = 0
+
     function setMarker(){
         if(!(box.childNodes.length > 0)){
-            let playerMarker = player.marker
+            let playerMarker = playerTurn()
+            roundNumber++
             box.textContent = playerMarker
         }else{
         return
     }
     }
 });
-
-
-
 
