775b2defe7f2d232b111a126153c8c7ea34dd6f2
jpblicer <jpblicer@gmail.com>
2023-11-05 20:49:58 +0900

    add eventlistener to each box. check if box is empty, then can click event add player marker, or else just return

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function drawGameBoard(){
-    gameBoard = []
+const player={
+    marker : "x",
 }
+
+const boxElements = document.querySelectorAll(".box");
+
+boxElements.forEach(box => {
+    box.addEventListener("click", setMarker);
+
+    function setMarker(){
+        if(!(box.childNodes.length > 0)){
+            let playerMarker = player.marker
+            box.textContent = playerMarker
+        }else{
+        return
+    }
+    }
+});
+
+
+
+
